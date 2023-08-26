ee5d19ea459d0e367a4f9229caf0871a861ea36a
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-08-26 13:08:51 +0800

    Initial Commit

---
+++ Cadaver/CadaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadaverApp.swift
+//  Cadaver
+//
+//  Created by Jia Chen Yee on 26/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CadaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cadaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cadaver
+//
+//  Created by Jia Chen Yee on 26/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
